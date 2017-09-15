e93751aee937633eca7ca9635bc69e2eb12af7a2
sonjasmith <sonjasmith@outlook.com>
2017-09-15 12:10:17 -0400

    First Commit

---
+++ HighStreetHospital/Doctor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighStreetHospital
+{
+    class Doctor : HospitalEmployee
+    {
+        //Fields
+        private string specialArea;
+        
+        //Properties 
+        public int NumberOfWheels
+        {
+            get { return this.; }
+        }
+        public Doctor()
+        {
+
+        }
+    }
+}
---
+++ HighStreetHospital/HospitalEmployee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighStreetHospital
+{
+    class HospitalEmpoyee
+    {
+        //Fields
+        protected string employeeName;
+        protected int employeeNumber;
+        protected int numberOfPatients;
+        protected string specialtyArea;
+        protected string department;
+        protected bool isOnPhone;
+        protected bool isSweeping;
+
+        //Properties
+        public string EmployeeName
+        {
+            get { return this.employeeName; }
+            set { this.employeeName = value; }
+        }
+        public int EmployeeNumber
+        {
+            get { return this.employeeNumber; }
+            set { this.employeeNumber = value; }
+        }
+        public int NumberOfPatients
+        {
+            get { return this.numberOfPatients; }
+            set { this.numberOfPatients = value; }
+        }
+        public string SpecialtyArea
+        {
+            get { return this.specialtyArea; }
+            set { this.specialtyArea = value; }
+        }
+        public string Department
+        {
+            get { return this.department; }
+            set { this.department = value; }
+        }
+        public bool IsOnPhone
+        {
+            get { return this.isOnPhone; }
+            set { this.isOnPhone = value; }
+        }
+        public bool IsSweeping
+        {
+            get { return this.isSweeping; }
+            set { this.isSweeping = value; }
+        }
+
+        //Constructors
+        public HospitalEmpoyee()
+        {
+
+        }
+        public HospitalEmpoyee()
+    }
+}
